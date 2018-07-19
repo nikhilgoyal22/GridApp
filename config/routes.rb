71a84c140e13b82ee1b4dff930c6b644cf00c990
Nikhil <nikhilgoyal22@gmail.com>
2018-07-19 21:21:49 +0530

    add index.html and its routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "home#index"
+
+  scope '/api' do
+    # my other api stuff
+  end
+
+  get '*unmatchedroute', to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
